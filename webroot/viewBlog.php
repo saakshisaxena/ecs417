eab975433a69eeee963868181e2154be13b4f72b
saakshisaxena <saakshi.saxena2000@gmail.com>
2020-05-04 16:53:17 +0100

    Update viewBlog.php

--- webroot/viewBlog.php
+++ webroot/viewBlog.php
@@ -1,9 +1,9 @@
 <?php
 
   //Make this connection as a separate conncetion file and then include once wherever needed!!!!!
-    $user = 'root';
-    $password='';
-    $db='miniprojectdb';
+    $user = 'user';
+    $password='password';
+    $db='ecs417';
 
     $conn = new mysqli('localhost', $user, $password, $db);
     //echo "gReat work :DD";
@@ -17,7 +17,7 @@
 
 <html>
   <head>
-    <link rel="alternate.stylesheet" type="text/css" href="reset.css" 
+    <link rel="alternate.stylesheet" type="text/css" href="reset.css"
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
